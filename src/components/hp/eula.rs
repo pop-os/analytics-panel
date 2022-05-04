750b8d276f77423402b5a6e50762784166d3fc79
Jeremy Soller <jackpot51@gmail.com>
2022-05-04 12:12:09 -0600

    Add hook to create TCO certified desktop icon to EULA.

--- src/components/hp/eula.rs
+++ src/components/hp/eula.rs
@@ -6,6 +6,51 @@ use crate::fl;
 use concat_in_place::strcat;
 use gtk::prelude::*;
 
+use std::{
+    env,
+    fs::OpenOptions,
+    io::Write,
+    os::unix::fs::OpenOptionsExt,
+    process::Command,
+};
+
+// Create TCO certified desktop file on supported hardware
+fn tco_certified() -> Result<(), String> {
+    let desktop_dir = match dirs::desktop_dir() {
+        Some(some) => some,
+        None => return Err(format!("failed to get desktop directory")),
+    };
+
+    let shortcut_path = desktop_dir.join("hp-tco-certified.desktop");
+    let shortcut_data = include_bytes!("../../../data/hp-tco-certified.desktop");
+    OpenOptions::new()
+        .create(true)
+        .write(true)
+        .mode(0o775)
+        .open(&shortcut_path)
+        .map_err(|err| {
+            format!("failed to open {}: {}", shortcut_path.display(), err)
+        })?
+        .write_all(shortcut_data).map_err(|err| {
+            format!("failed to write {}: {}", shortcut_path.display(), err)
+        })?;
+
+    let status = Command::new("gio")
+        .arg("set")
+        .arg(&shortcut_path)
+        .arg("metadata::trusted")
+        .arg("true")
+        .status()
+        .map_err(|err| {
+            format!("failed to set {} as trusted: {}", shortcut_path.display(), err)
+        })?;
+    if ! status.success() {
+        return Err(format!("failed to set {} as trusted: {}", shortcut_path.display(), status));
+    }
+
+    Ok(())
+}
+
 pub struct Model {}
 
 #[derive(relm_derive::Msg)]
@@ -22,6 +67,13 @@ impl relm::Widget for Widget {
             let eula_markup = include_str!("../../../data/hp-eula.md");
             buffer.insert_markup(&mut buffer.start_iter(), eula_markup);
         });
+
+        match tco_certified() {
+            Ok(()) => (),
+            Err(err) => {
+                eprintln!("failed to create TCO certified link: {}", err);
+            }
+        }
     }
 
     fn model(_relm: &relm::Relm<Self>, _: ()) -> Model {
